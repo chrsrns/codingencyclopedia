7a6bb5bca0948259ff8eda5dadce8f3d37260c14
Christian Aranas <aranaschristianlouise@gmail.com>
2024-04-20 18:03:38 +0800

    Changed `SignUpPage` preview implementation
    Made `onSignUpSwitchClicked` required.
    Moved previews to separate private functions so that the component itself isn't wrapped in the theme and surface composables.

--- app/src/main/java/com/chrsrns/codingencyclopedia/ui/pages/SignUpPage.kt
+++ app/src/main/java/com/chrsrns/codingencyclopedia/ui/pages/SignUpPage.kt
@@ -36,11 +36,9 @@ import com.chrsrns.codingencyclopedia.Utilities.validateEmail
 import com.example.compose.CodingEncyclopediaTheme
 import kotlinx.coroutines.launch
 
-@Preview(showBackground = true)
-@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun SignUpPage(
-    onSignUpSwitchClicked: () -> Unit = {}
+    onSignUpSwitchClicked: () -> Unit
 ) {
     val context = LocalContext.current
     val scope = rememberCoroutineScope()
@@ -51,90 +49,104 @@ fun SignUpPage(
     var signUpResultMsg by remember { mutableStateOf("") }
     val successMsg = "Sign up successful."
 
-    CodingEncyclopediaTheme {
-        Surface {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(32.dp)
-            ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        "Sign Up",
-                        fontSize = MaterialTheme.typography.headlineLarge.fontSize,
-                        fontWeight = FontWeight(700)
-                    )
-                    Text(
-                        "to create your own account", style = TextStyle(
-                            fontSize = 20.sp,
-                            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.85f)
-                        )
-                    )
-                }
-                Spacer(modifier = Modifier.height(32.dp))
-                if (signUpResultMsg == successMsg) {
-                    Text(
-                        successMsg,
-                        fontSize = 12.sp,
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
-                } else if (signUpResultMsg != "") {
-                    Text(
-                        signUpResultMsg,
-                        fontSize = 12.sp,
-                        color = Color.Red
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
-                }
-                OutlinedTextField(
-                    modifier = Modifier.fillMaxWidth(),
-                    value = username,
-                    onValueChange = {
-                        username = it
-                        signUpResultMsg = ""
-                    },
-                    label = { Text("Enter your name here") })
-                Spacer(modifier = Modifier.height(24.dp))
-                OutlinedTextField(
-                    modifier = Modifier.fillMaxWidth(),
-                    value = email,
-                    onValueChange = {
-                        email = it
-                        signUpResultMsg = ""
-                    },
-                    label = { Text("Enter your email address here") })
-                Spacer(modifier = Modifier.height(20.dp))
-                Column (Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Button(
-                        onClick = {
-                            scope.launch {
-                                if (!(validateEmail(email.text))) {
-                                    signUpResultMsg = "Email is invalid"
-                                    AppDatabase
-                                        .getInstance(context)
-                                        .userDao().insertAll(
-                                            User(
-                                                name = username.text,
-                                                email = email.text,
-                                            )
-                                        )
-                                } else signUpResultMsg = successMsg
-                            }
-                        }) {
-                        Text("Sign in", fontSize = MaterialTheme.typography.bodyLarge.fontSize)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "Sign Up",
+                fontSize = MaterialTheme.typography.headlineLarge.fontSize,
+                fontWeight = FontWeight(700)
+            )
+            Text(
+                "to create your own account", style = TextStyle(
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.85f)
+                )
+            )
+        }
+        Spacer(modifier = Modifier.height(32.dp))
+        if (signUpResultMsg == successMsg) {
+            Text(
+                successMsg,
+                fontSize = 12.sp,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+        } else if (signUpResultMsg != "") {
+            Text(
+                signUpResultMsg,
+                fontSize = 12.sp,
+                color = Color.Red
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = username,
+            onValueChange = {
+                username = it
+                signUpResultMsg = ""
+            },
+            label = { Text("Enter your name here") })
+        Spacer(modifier = Modifier.height(24.dp))
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = email,
+            onValueChange = {
+                email = it
+                signUpResultMsg = ""
+            },
+            label = { Text("Enter your email address here") })
+        Spacer(modifier = Modifier.height(20.dp))
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(
+                onClick = {
+                    scope.launch {
+                        if (!(validateEmail(email.text))) {
+                            signUpResultMsg = "Email is invalid"
+                            AppDatabase
+                                .getInstance(context)
+                                .userDao().insertAll(
+                                    User(
+                                        name = username.text,
+                                        email = email.text,
+                                    )
+                                )
+                        } else signUpResultMsg = successMsg
                     }
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Text(text = "Already have an account?", fontSize = MaterialTheme.typography.bodyLarge.fontSize)
-                        TextButton(onClick = { onSignUpSwitchClicked() }) {
-                            Text("Sign in", fontSize = MaterialTheme.typography.bodyLarge.fontSize)
-                        }
-                    }
+                }) {
+                Text("Sign in", fontSize = MaterialTheme.typography.bodyLarge.fontSize)
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = "Already have an account?",
+                    fontSize = MaterialTheme.typography.bodyLarge.fontSize
+                )
+                TextButton(onClick = { onSignUpSwitchClicked() }) {
+                    Text("Sign in", fontSize = MaterialTheme.typography.bodyLarge.fontSize)
                 }
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SignUpPagePreview() {
+    CodingEncyclopediaTheme {
+        Surface {
+            SignUpPage(onSignUpSwitchClicked = {})
+        }
+    }
+}
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun SignUpPagePreview_Dark() {
+    SignUpPagePreview()
 }
